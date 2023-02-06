b337b88e86556cb6f1b0b54fd6c21e93d05f78f2
Jinseop-Sim <jjanggu1565@gmail.com>
2023-02-06 23:40:32 +0900

    fix : API 문서화를 위한 Security Config 수정

--- Backend/src/main/java/bera31/Project/config/security/SecurityConfig.java
+++ Backend/src/main/java/bera31/Project/config/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
-                .antMatchers("/api/auth/**", "/").permitAll()
+                .antMatchers("/api/auth/**", "/", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
