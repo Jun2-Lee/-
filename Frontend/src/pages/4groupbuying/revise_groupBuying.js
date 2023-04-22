94a802d8814d2ab1e324bcafcee1032e0b2c5f4e
Jisu Lee <dlwltn0430@naver.com>
2023-04-23 00:26:13 +0900

    feat: 공동구매 글 수정하기 구현

--- Frontend/src/pages/4groupbuying/revise_groupBuying.js
+++ Frontend/src/pages/4groupbuying/revise_groupBuying.js
@@ -1,11 +1,66 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import ImgUpload from '../../components/imgUpload'
 import AddressSelect from '../../components/addressSelect'
 import './revise_groupBuying.css'
 
 export default function RevisegroupBuying() {
+    const [data, setData] = useState({});
+    const { postId } = useParams();
+    const [title, setTitle] = useState('')
+    const [category, setCategory] = useState('')
+    const [product, setProduct] = useState('')
+    const [link, setLink] = useState('')
+    const [price, setPrice] = useState('')
+    const [memberLimit, setMemberLimit] = useState('')
+    const [deadLine, setDeadLine] = useState('')
+    const [dong, setDong] = useState('')
+    const [gu, setGu] = useState('')
+    const [content, setContent] = useState('')
+
+    const [groupBuyingRequestDto, setGroupBuyingRequestDto] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://3.36.144.128:8080/api/groupBuying/${postId}`);
+        const data = response.data;
+        setData(data)
+        setTitle(data.title)
+        setCategory(data.category)
+        setProduct(data.product)
+        setLink(data.link)
+        setPrice(data.price)
+        setMemberLimit(data.memberLimit)
+        setDeadLine(data.deadLine)
+        setDong(data.dong)
+        setGu(data.gu)
+        setContent(data.content)
+        setPostImage(null)
+
+        setGroupBuyingRequestDto({
+          title: data.title,
+          category: data.category,
+          product: data.product,
+          link: data.link,
+          price: data.price,
+          memberLimit: data.memberLimit,
+          deadLine: data.deadLine,
+          dong: data.dong,
+          gu: data.gu,
+          content: data.content,
+          image: data.postImage 
+        })
+        setPostImage(null)
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
   //음식 카테고리 선택
   const foodTypes = ['채소', '과일/견과/쌀', '수산물/건해산', '정육/계란', '우유/유제품', '면류/통조림', '샐러드/간편식', '양념', '생수/음료', '간식/과자/떡']
   const [selectedFood, setSelectedFood] = useState('');
@@ -16,6 +71,10 @@ export default function RevisegroupBuying() {
       ...groupBuyingRequestDto,
       category: e.target.value
     })
+    setData({
+      ...data,
+      category: e.target.value
+    });
   }
 
   //이미지 업로드
@@ -24,6 +83,7 @@ export default function RevisegroupBuying() {
 
   function handleImg(selectedImgValue) {
     setPostImage(selectedImgValue)
+    //setImgsrc(data.postImage)
     setImgsrc(URL.createObjectURL(selectedImgValue))
   }
 
@@ -31,12 +91,18 @@ export default function RevisegroupBuying() {
   const [selectedGu, setSelectedGu] = useState('');
   const [selectedDong, setSelectedDong] = useState('');
 
-  function handleSelectedGu(selectedGuValue) {
+  function handleSelectedGu({selectedGuValue}) {
     setSelectedGu(selectedGuValue)
     setGroupBuyingRequestDto({
       ...groupBuyingRequestDto,
       gu: selectedGuValue
     })
+    /*
+    setData({
+      ...data,
+      gu: selectedGuValue
+    });
+    */
   }
 
   function handleSelectedDong(selectedDongValue) {
@@ -45,6 +111,12 @@ export default function RevisegroupBuying() {
       ...groupBuyingRequestDto,
       dong: selectedDongValue
     })
+    /*
+    setData({
+      ...data,
+      dong: selectedDongValue
+    });
+    */
   }
 
   //axios header 선언
@@ -52,23 +124,12 @@ export default function RevisegroupBuying() {
     'Content-Type': 'multipart/form-data'
   }
 
-  const [groupBuyingRequestDto, setGroupBuyingRequestDto] = useState({
-    title: '',
-    category: '',
-    product: '',
-    link: '',
-    price: '',
-    memberLimit: '',
-    deadLine: '',
-    dong: '',
-    gu: '',
-    content: ''
-  });
-
-  const { title, category, product, link, price, memberLimit, deadLine, dong, gu, content } = groupBuyingRequestDto;
-
   const onChange = (e) => {
     const { value, name } = e.target;
+    setData({
+      ...data,
+      [name]: value
+    });
     setGroupBuyingRequestDto({
       ...groupBuyingRequestDto,
       [name]: value
@@ -87,12 +148,6 @@ export default function RevisegroupBuying() {
     }
     else if (product.length === 0) {
       alert("상품명을 입력해주세요")
-    }
-    /*else if (link.length === 0) {
-      alert("링크를 입력해주세요")
-    }*/
-    else if (postImage.length === 0) {
-      alert("사진을 업로드 해주세요")
     }
     else if (price.length === 0) {
       alert("공동구매 비용을 입력해주세요")
@@ -121,8 +176,11 @@ export default function RevisegroupBuying() {
       form.append('groupBuyingRequestDto', new Blob([JSON.stringify(groupBuyingRequestDto)], {
         type: "application/json"
       }))
+      
 
-      axios.post("http://3.36.144.128:8080/api/groupBuying", form, {headers})
+      const accessToken = localStorage.getItem('accessToken')
+      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+      axios.put(`http://3.36.144.128:8080/api/groupBuying/${postId}`, form, {headers})
         .then(function(response) {
           console.log(response)
           alert("수정되었습니다")
@@ -132,18 +190,18 @@ export default function RevisegroupBuying() {
         })
       }
     }
-
+    
     return (
       <div className='groupBuying_container'>
         <form onSubmit={onSubmit}>
           <div className="Title">
             <label className="form-label">제목</label>
-            <input name='title' onChange={onChange} value={title} className="title"/>
+            <input name='title' onChange={onChange} value={data.title} className="title" />
           </div>
           
           <div className="product_classification">
             <label className="form-label">카테고리</label>
-            <select name={category} onChange={handleFoodChange} value={selectedFood} className="category">
+            <select name={category} onChange={handleFoodChange} value={data.category} className="category">
               {foodTypes.map((foodType) => (
               <option value={foodType} key={foodType}> {foodType} </option>
               ))}
@@ -152,12 +210,12 @@ export default function RevisegroupBuying() {
 
           <div className="ProductName">
             <label className="form-label">상품명</label>
-            <input name='product' onChange={onChange} value={product} className="productName"/>
+            <input name='product' onChange={onChange} value={data.product} className="productName" />
           </div>
 
           <div className="Link">
             <label className="form-label">링크</label>
-            <input name='link' onChange={onChange} value={link} className="link"/>
+            <input name='link' onChange={onChange} value={data.link} className="link" />
           </div>
 
           <div className="productImg_upload">
@@ -167,19 +225,19 @@ export default function RevisegroupBuying() {
 
           <div className="TotalGroupBuying">
             <label className="form-label">공동구매 비용</label>
-            <input name='price' onChange={onChange} value={price} className="totalGroupBuying" />
+            <input name='price' onChange={onChange} value={data.price} className="totalGroupBuying" />
             <label id="won">원</label>
           </div>
 
           <div className="NumPeople_groupBuying">
             <label className="form-label">모집인원</label>
-            <input name='memberLimit' onChange={onChange} value={memberLimit} className="numPeople_groupBuying" />
+            <input name='memberLimit' onChange={onChange} value={data.memberLimit} className="numPeople_groupBuying" />
             <label id="myeong">명</label>
           </div>
 
           <div className="Deadline_groupBuying">
             <label className="form-label">마감일</label>
-            <input name='deadLine' onChange={onChange} value={deadLine} type="datetime-local" className="deadline" />
+            <input name='deadLine' onChange={onChange} value={data.deadLine} type="datetime-local" className="deadline" />
           </div>
 
           <div className="address">
@@ -189,11 +247,11 @@ export default function RevisegroupBuying() {
 
           <div className="Explanation">
             <label className="form-label">설명(기타사항)</label>
-            <input name='content' onChange={onChange} value={content} className="explanation" />
+            <input name='content' onChange={onChange} value={data.content} className="explanation" />
           </div>
 
           <div className="submit">
-            <input type="submit" className="post_delivery" value="등록하기" />
+            <input type="submit" className="post_delivery" value="수정하기" />
           </div>
         </form>
       </div>
