d8747b12167b8a278dc4ef2b84cb1192ec7774fb
Jisu Lee <dlwltn0430@naver.com>
2023-04-25 12:45:31 +0900

    feat: n빵 글 등록 기능 구현

--- Frontend/src/pages/3dutchpay/post_dutchpay.js
+++ Frontend/src/pages/3dutchpay/post_dutchpay.js
@@ -24,14 +24,15 @@ function PostDutchpay() {
   const [dutchpayRequestDto, setDutchpayRequestDto] = useState({
     store: '',
     category: '',
-    /* 배달 주소 */
+    address: '',
+    detailAddress: '',
     deliveryCost: '',
     limitMember: '',
     deadLine: '',
     content: '',
   });
 
-  const { store, category, address, deliveryCost, limitMember, deadLine, content } = dutchpayRequestDto;
+  const { store, category, address, detailAddress, deliveryCost, limitMember, deadLine, content } = dutchpayRequestDto;
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -40,22 +41,76 @@ function PostDutchpay() {
       [name]: value
     })
   };
+  /*
+  const handleAddress1 = (e) => {
+    setAddress1(e.target.value)
+    setDutchpayRequestDto({
+      ...dutchpayRequestDto,
+      address: e.target.value + " " + address2
+    })
+  }
 
+  const handleAddress2 = (e) => {
+    setAddress2(e.target.value)
+    setDutchpayRequestDto({
+      ...dutchpayRequestDto,
+      address: address1 + " " + e.target.value
+    })
+  }
+  */
+  console.log(address)
   const navigate = useNavigate();
   const onSubmit = (e) => {
     e.preventDefault();
 
-    let form = new FormData()
-    //form.append 채우기
-
-    axios.post("http://3.36.144.128:8080/api/dutchPay", form, {headers})
-      .then(function(response) {
-        console.log(response)
-        navigate('/dutchpay')
-      }) .catch(function(error) {
-        console.log(error)
-      })
+    if (store.length === 0) {
+      alert("매장 이름을 입력해주세요")
     }
+    else if (category.length === 0) {
+      alert("카테고리를 선택해주세요")
+    }
+    else if (address.length === 0) {
+      alert("배달 주소를 입력해주세요")
+    }
+    else if (detailAddress.length === 0) {
+      alert("상세 주소를 입력해주세요")
+    }
+    else if (deliveryCost.length === 0) {
+      alert("총 배달비를 입력해주세요")
+    }
+    else if (isNaN(deliveryCost)) {
+      alert("총 배달비를 숫자로 입력해주세요")
+    }
+    else if (limitMember.length === 0) {
+      alert("모집인원을 입력해주세요")
+    }
+    else if (isNaN(deliveryCost)) {
+      alert("모집인원을 숫자로 입력해주세요")
+    }
+    else if (deadLine.length === 0) {
+      alert("마감일을 입력해주세요")
+    }
+    else {
+      /*let form = new FormData()
+      form.append("store", store)
+      form.append("category", category)
+      form.append("address", address)
+      form.append("deliveryCost", deliveryCost)
+      form.append("limitMember", limitMember)
+      form.append("deadLine", deadLine)
+      form.append("content", content)*/
+
+      axios.post("http://3.36.144.128:8080/api/dutchPay", dutchpayRequestDto, {headers})
+        .then(function(response) {
+          console.log(response)
+          alert("등록되었습니다")
+          navigate('/dutchpay')
+        }) .catch(function(error) {
+          console.log(error)
+          console.log(dutchpayRequestDto)
+        })
+    }
+  }
 
     return (
       <div className='postDutchpay_container'>
@@ -76,14 +131,12 @@ function PostDutchpay() {
 
           <div className="DeliveryAddress">
             <label className="form-label">배달 주소</label>
-            <input input name='address' onChange={onChange} value={address} className="deliveryAddress" />
+            <input name='address' onChange={onChange} value={address} className="deliveryAddress" />
             <button type="submit" className="searchAddress">주소 검색</button>
           </div>
 
           <div className="DetailedAddress">
-            <input 
-              className="detailedAddress" placeholder="상세 주소"
-              />
+            <input name='detailAddress' onChange={onChange} value={detailAddress} className="detailedAddress" placeholder="상세 주소" />
           </div>
 
           <div className="TotalDelivery">
