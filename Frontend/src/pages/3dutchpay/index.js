3acd52b8961231c67c2babf9c441f70a326b85f1
Jisu Lee <dlwltn0430@naver.com>
2023-05-04 18:21:56 +0900

    feat: 배달비 n빵 상세보기 기능 구현

--- Frontend/src/pages/3dutchpay/index.js
+++ Frontend/src/pages/3dutchpay/index.js
@@ -1,17 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from 'react-router-dom'
 import './index.css';
+import axios from 'axios'
 
 const { kakao } = window;
 
 function DutchPayPage() {
-  /*useEffect(() => {
+  const [data, setData] = useState([]);
+  const [detail, setDetail] = useState([]);
+  const [showDiv, setShowDiv] = useState(false);
+  const [dutchPayId, setDutchPayId] = useState('');
+  const accessToken = localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    //카카오맵 보여주기
     var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     var options = { //지도를 생성할 때 필요한 기본 옵션
       center: new kakao.maps.LatLng(35.229609, 129.089358), //지도의 중심좌표.
       level: 3 //지도의 레벨(확대, 축소 정도)
     };
     var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-  }, [])*/
+
+    axios.get('http://3.36.144.128:8080/api/dutchPay')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => console.log(error));
+  }, [])
+
+  function handleDetail(id) {
+    //setShowDiv(!showDiv)
+    setShowDiv(true);
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+    axios.get(`http://3.36.144.128:8080/api/dutchPay/${id}`)
+        .then(response => {
+          setDetail(response.data)
+          console.log(response.data)
+          console.log(dutchPayId)
+        })
+        .catch(error => console.log(error)); 
+  }
 
   return (
     <div className='dutch'>
@@ -25,94 +54,50 @@ function DutchPayPage() {
           <option value="고기·구이">고기·구이</option>
         </select>
 
-        <div className="card_dutchpay">
+      {data.map((item, index) => (
+        <div key ={index} className="card_dutchpay" onClick={() => handleDetail(item.id)}>
           <div className="foodIcon">
-            <img className="f1" src="assets/img/dutchpay/pig_hocks.png"/>
-            <label id="foodName">족발</label>
+            {item.category === '족발/보쌈' && <img className="f1" src="assets/img/dutchpay/pig_hocks.png"/>}
+            {item.category === '찜/탕/찌개' && <img className="f1" src="assets/img/dutchpay/stew.png"/>}
+            {item.category === '돈까스/회/일식' && <img className="f1" src="assets/img/dutchpay/japanese_food.png"/>}
+            {item.category === '피자' && <img className="f1" src="assets/img/dutchpay/pizza.png"/>}
+            {item.category === '고기/구이' && <img className="f1" src="assets/img/dutchpay/meat.png"/>}
+            {/*여기에 카테고리 더 추가해야 됨! */}
+            <label id="foodName">{item.category}</label>
           </div>
           <div className="card_sub1">
-            <div className="title_dutchpay">제목</div>
+            <div className="title_dutchpay">{item.store}</div>
             <div className="card_sub2">
-              <div className="dong_dutchpay">장전동</div>
+              <div className="dong_dutchpay">{item.address}</div>
               <div className="deadline_dutchpay">00:03:00</div>
             </div>
           </div>
         </div>
+      ))}
+    </div>
 
-        <div className="card_dutchpay">
-          <div className="foodIcon">
-            <img className="f2" src="assets/img/dutchpay/stew.png"/>
-            <label id="foodName">찌개</label>
-          </div>
-          <div className="card_sub1">
-            <div className="title_dutchpay">제목</div>
-            <div className="card_sub2">
-              <div className="dong_dutchpay">장전동</div>
-              <div className="deadline_dutchpay">00:03:00</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="card_dutchpay">
-          <div className="foodIcon">
-            <img className="f3" src="assets/img/dutchpay/japanese_food.png"/>
-            <label id="foodName">일식</label>
-          </div>
-          <div className="card_sub1">
-            <div className="title_dutchpay">제목</div>
-            <div className="card_sub2">
-              <div className="dong_dutchpay">장전동</div>
-              <div className="deadline_dutchpay">00:03:00</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="card_dutchpay">
-          <div className="foodIcon">
-            <img className="f4" src="assets/img/dutchpay/pizza.png"/>
-            <label id="foodName">피자</label>
-          </div>
-          <div className="card_sub1">
-            <div className="title_dutchpay">제목</div>
-            <div className="card_sub2">
-              <div className="dong_dutchpay">장전동</div>
-              <div className="deadline_dutchpay">00:03:00</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="card_dutchpay">
-          <div className="foodIcon">
-            <img className="f5" src="assets/img/dutchpay/meat.png"/>
-            <label id="foodName">고기/구이</label>
-          </div>
-          <div className="card_sub1">
-            <div className="title_dutchpay">제목</div>
-            <div className="card_sub2">
-              <div className="dong_dutchpay">장전동</div>
-              <div className="deadline_dutchpay">00:03:00</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+    {showDiv && (
       <div className="detail_dutchpay">
         <div className="userInfo_dutchpay">
           <img className="profile_dutchpay" src="assets/img/default_profile.png"/>
-          <span id="nickName_dutchpay">뭐먹지님</span>
+          <span id="nickName_dutchpay">{detail.nickname}</span>
         </div>
-        <div className="storeName_dutchpay">매장 이름</div>
+        <div className="storeName_dutchpay">{detail.store}</div>
         <div className="detail_sub1">
           <div className="foodImg_dutchpay">
-            <img src="assets/img/dutchpay/돈까스,회,일식.png"/>
+            {detail.category === '족발/보쌈' && <img className="f1" src="assets/img/dutchpay/pig_hocks.png"/>}
+            {detail.category === '찜/탕/찌개' && <img className="f1" src="assets/img/dutchpay/stew.png"/>}
+            {detail.category === '돈까스/회/일식' && <img className="f1" src="assets/img/dutchpay/japanese_food.png"/>}
+            {detail.category === '피자' && <img className="f1" src="assets/img/dutchpay/pizza.png"/>}
+            {detail.category === '고기/구이' && <img className="f1" src="assets/img/dutchpay/meat.png"/>}
           </div>
           <div className="detail_sub2">
-            <div className="recruits_dutchpay">n명 모집</div>
+            <div className="recruits_dutchpay">{detail.limitMember}명 모집</div>
             <div className="calcCost_dutchpay"></div>
             <div className="countdown_dutchpay">카운트 다운</div>
           </div>
         </div>
-        <div className="address_dutchpay">00시 00구 00동 000호</div>
+        <div className="address_dutchpay">{detail.address}</div>
         <div className="explanation_dutchpay"></div>
         <div className="UserHelp_dutchpay">
           <a href="#" id="modify_dutchpay">수정하기</a>
@@ -123,9 +108,10 @@ function DutchPayPage() {
               <input type="submit" className="signup_dutchpay" value="신청하기"></input>
         </div>
       </div>
-
-      <div id="map"></div>
-    </div>
+    )}
+     
+    <div id="map"></div>
+  </div>
   )
 }
 
