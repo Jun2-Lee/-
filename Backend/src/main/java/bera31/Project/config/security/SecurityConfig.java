b864ca2a47ec87794bab43a56f0c1c05ea9c9bf6
Jinseop-Sim <jjanggu1565@gmail.com>
2023-05-04 23:12:43 +0900

    fix : SecurityConfig 수정

--- Backend/src/main/java/bera31/Project/config/security/SecurityConfig.java
+++ Backend/src/main/java/bera31/Project/config/security/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
                 .antMatchers("/api/auth/**", "/", "/swagger-ui/**",
                         "/v3/api-docs", "/swagger-resources/**", "/naong-api",
                         "/swagger-ui.html", "/api/groupBuying", "/api/groupBuying/page/**","/api/sharing/page/**",
+                        "/api/groupBuying/{\\d+}", "/api/sharing/{\\d+}", "/api/dutchPay/{\\d+}",
                         "/api/sharing", "/api/dutchPay", "/index.html",
                         // 여기 아래로는 Frontend에서 발생하는 Unauthorized(401)에 대한 permission입니다.
                         "/favicon.ico", "/css/**", "/fonts/**", "/img/**", "/js/**",
