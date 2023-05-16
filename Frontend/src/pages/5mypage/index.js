6e20310c65da72627984418f96cd8c796838d11c
Jisu Lee <dlwltn0430@naver.com>
2023-05-17 01:51:05 +0900

    feat: 5 마이페이지 쪽지 제외 api 요청 코드 완성

--- Frontend/src/pages/5mypage/index.js
+++ Frontend/src/pages/5mypage/index.js
@@ -5,38 +5,73 @@ import "./index.css";
 import axios from 'axios'
 
 function MyPage() {
-  const [items, setItems] = useState([]);
+  const[data, setData] = useState('');
 
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}.${month}.${day}`;
+  }
+  
   useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken')
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
     axios.get('http://3.36.144.128:8080/api/mypage')
       .then(response => {
-        setItems(Object.values(response.data).map(item => {
-          const date_sharing = new Date(item.simpleSharingList.postTime).toLocaleDateString("ko-KR");
-          const date_dutchpay = new Date(item.simpleDutchPayList.postTime).toLocaleDateString("ko-KR");
-          const date_groupBuying = new Date(item.simpleGroupBuyingList.postTime).toLocaleDateString("ko-KR");
-          return { ...item, 
-                  "simpleSharingList": { ...item.simpleSharingList, "postTime": date_sharing },
-                  "simpleDutchPayList": { ...item.simpleDutchPayList, "postTime": date_dutchpay },
-                  "simpleGroupBuyingList": { ...item.simpleGroupBuyingList, "postTime": date_groupBuying },
-                }     
-        }));
-      })
+        setData(
+          {
+            ...response.data,
+            simpleDutchPayList: response.data.simpleDutchPayList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            })),
+            simpleGroupBuyingList: response.data.simpleGroupBuyingList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            })),
+            simpleLikedGroupBuyingList: response.data.simpleLikedGroupBuyingList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            })),
+            simpleLikedSharingList: response.data.simpleLikedSharingList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            })),
+            simpleParticipantingDutchPayList: response.data.simpleParticipantingDutchPayList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            })),
+            simpleParticipantingGroupBuyingList: response.data.simpleParticipantingGroupBuyingList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            })),
+            simpleSharingList: response.data.simpleSharingList.map((item) => ({
+              ...item,
+              postTime: formatDate(item.postTime),
+            }))
+          });
+        })
       .catch(error => console.log(error));
   }, [])
+  console.log(data)
 
-  function handeleSimpleSharingList() {
-    
-  }
-  /*useEffect(() => {
-    axios.get('http://3.36.144.128:8080/api/mypage')
-      .then(response => {
-        setItems(response.data.map(item => {
-          const formattedDate_sharing = new Date(item.postTime).toLocaleDateString("ko-KR");
-          return { ...item, postTime: formattedDate };
-        }));
-      })
-      .catch(error => console.log(error));
-  }, [])*/
+  const [isSharingClicked, setSharing] = useState(true);
+  const [isDutchpayClicked, setDutchpay] = useState(false);
+  const [isGroupbuyingClicked, setGroupbuying] = useState(false);
+
+  const [isDutchpayClicked2, setDutchpay2] = useState(true);
+  const [isGroupbuyingClicked2, setGroupbuying2] = useState(false);
+
+  const [isSharingClicked3, setSharing3] = useState(true);
+  const [isGroupbuyingClicked3, setGroupbuying3] = useState(false);
+  
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const date = now.getDate().toString().padStart(2, '0');
+  
   return (
     <div className="mypage_body">
       <section className="mypage_categoryBar">
@@ -48,8 +83,8 @@ function MyPage() {
           <label id="mypage_label">내 프로필</label>
 
           <div className="mypage_profile">
-            <img src="assets/img/bigLogo.png"/>
-            <span>뭐먹지님, 안녕하세요</span>
+            <img src={data.image}/>
+            <span>{data.nickname}님, 안녕하세요</span>
           </div>
 
           <div className="modify_profile">
@@ -58,11 +93,30 @@ function MyPage() {
         </div>
 
         <div className="mypage_container">
-          <label id="mypage_label">참여 목록</label>
-          <div className="participation_list"></div>
-          <div className="participation_list"></div>
-          <div className="participation_list"></div>
-          <div className="participation_list"></div>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <label id="mypage_label">신청 목록</label>
+            <span className="mypost">
+              <span onClick={() => {setDutchpay2(true); setGroupbuying2(false);}}>&nbsp;배달비 n빵&nbsp;</span> |
+              <span onClick={() => {setDutchpay2(false); setGroupbuying2(true);}}>&nbsp;공동구매</span>
+            </span>
+          </div>
+          {isDutchpayClicked2 && data.simpleParticipantingDutchPayList && data.simpleParticipantingDutchPayList.map((item, index) => (
+            <div key={index}>
+              <div className="participation_list">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
+            </div>
+          ))}
+
+          {isGroupbuyingClicked2 && data.simpleParticipantingGroupBuyingList && data.simpleParticipantingGroupBuyingList.map((item, index) => (
+            <div key={index}>
+              <div className="participation_list">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className="mypage_container">
@@ -74,25 +128,66 @@ function MyPage() {
         </div>
 
         <div className="mypage_container">
-          <label id="mypage_label">찜 목록</label>
-          <div className="scrap_list">
-            <div className="box1"></div>
-            <div className="box2"></div>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <label id="mypage_label">찜 목록</label>
+            <span className="mypost">
+              <span onClick={() => {setSharing3(true); setGroupbuying3(false);}}>&nbsp;재료나눔&nbsp;</span> |
+              <span onClick={() => {setSharing3(false); setGroupbuying3(true);}}>&nbsp;공동구매</span>
+            </span>
           </div>
-          <div className="scrap_list">
-            <div className="box1"></div>
-            <div className="box2"></div>
-          </div>
-          
+          {isSharingClicked3 && data.simpleLikedSharingList && data.simpleLikedSharingList.map((item, index) => (
+            <div key={index}>
+              <div className="participation_list">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
+            </div>
+          ))}
+
+          {isGroupbuyingClicked3 && data.simpleLikedGroupBuyingList && data.simpleLikedGroupBuyingList.map((item, index) => (
+            <div key={index}>
+              <div className="participation_list">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className="mypage_container">
-          <label id="mypage_label">내가 쓴 글</label>
-          <span className="mypost">재료나눔 | 배달비 n빵 | 공동구매</span>
-          {items.map((item, index) => (
-            <div key={index} className="scrap_list">
-              <div className="box1">{item.simpleSharingList[0].title}</div>
-              <div className="box2">{item.simpleSharingList[1].title}</div>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <label id="mypage_label">내가 쓴 글</label>
+            <span className="mypost">
+              <span onClick={() => {setSharing(true); setDutchpay(false); setGroupbuying(false);}}>재료나눔&nbsp;</span> | 
+              <span onClick={() => {setSharing(false); setDutchpay(true); setGroupbuying(false);}}>&nbsp;배달비 n빵&nbsp;</span> |
+              <span onClick={() => {setSharing(false); setDutchpay(false); setGroupbuying(true);}}>&nbsp;공동구매</span>
+            </span>
+          </div>
+          
+          {isSharingClicked && data.simpleSharingList && data.simpleSharingList.map((item, index) => (
+            <div key={index}>
+              <div className="my_writing">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
+            </div>
+          ))}
+
+          {isDutchpayClicked && data.simpleDutchPayList && data.simpleDutchPayList.map((item, index) => (
+            <div key={index}>
+              <div className="my_writing">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
+            </div>
+          ))}
+
+          {isGroupbuyingClicked && data.simpleGroupBuyingList && data.simpleGroupBuyingList.map((item, index) => (
+            <div key={index}>
+              <div className="my_writing">
+                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
+              </div>
             </div>
           ))}
         </div>
@@ -100,13 +195,18 @@ function MyPage() {
         <div className="mypage_container">
           <label id="mypage_label">캘린더</label>
           <div className="calendar_container">
-            <div className="mydate"></div>
-            <div className="schedule_container">
-              <div className="schedule_list"></div>
-              <div className="schedule_list"></div>
-              <div className="schedule_list"></div>
-              <div className="schedule_list"></div>
+            <div className="mydate">
+              <div style={{height: '104px'}}>
+                <div style={{marginTop: '9px', textAlign: 'center', fontSize: '14px'}}>{year}년 {month}월</div>
+                <div style={{display: 'flex', alignItems: 'center', height: '80px', textAlign: 'center', fontSize: '64px'}}>{date}</div>
+              </div>
             </div>
+
+            {data.todayScheduleList && data.todayScheduleList.map((item, index) => (
+              <div key={index} className="schedule_container">
+                <div className="schedule_list">{item.title}</div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
