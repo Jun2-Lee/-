c335f7b37b67e2877f4c799a3cc2db7dc723b66f
Jisu Lee <dlwltn0430@naver.com>
2023-05-17 21:54:55 +0900

    feat: 마이페이지에서 게시물 제목 누르면 해당 게시물로 이동하는 기능 구현

--- Frontend/src/pages/5mypage/index.js
+++ Frontend/src/pages/5mypage/index.js
@@ -1,5 +1,6 @@
-import {useState} from "react";
+import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import CategoryBar2 from "../../components/categoryBar2";
 import "./index.css";
 import axios from 'axios'
@@ -88,7 +89,7 @@ function MyPage() {
           </div>
 
           <div className="modify_profile">
-            <span>프로필 수정하기</span>
+            <Link to="/editProfile">프로필 수정하기</Link>
           </div>
         </div>
 
@@ -112,7 +113,9 @@ function MyPage() {
           {isGroupbuyingClicked2 && data.simpleParticipantingGroupBuyingList && data.simpleParticipantingGroupBuyingList.map((item, index) => (
             <div key={index}>
               <div className="participation_list">
-                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '70%', fontSize: '11px'}}>
+                  <Link to={`/groupBuying/${item.id}`}>{item.title}</Link>
+                </div>
                 <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
               </div>
             </div>
@@ -138,7 +141,9 @@ function MyPage() {
           {isSharingClicked3 && data.simpleLikedSharingList && data.simpleLikedSharingList.map((item, index) => (
             <div key={index}>
               <div className="participation_list">
-                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '70%', fontSize: '11px'}}>
+                  <Link to={`/sharing/${item.id}`}>{item.title}</Link>
+                </div>
                 <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
               </div>
             </div>
@@ -147,7 +152,9 @@ function MyPage() {
           {isGroupbuyingClicked3 && data.simpleLikedGroupBuyingList && data.simpleLikedGroupBuyingList.map((item, index) => (
             <div key={index}>
               <div className="participation_list">
-                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '70%', fontSize: '11px'}}>
+                  <Link to={`/groupBuying/${item.id}`}>{item.title}</Link>
+                </div>
                 <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
               </div>
             </div>
@@ -167,7 +174,9 @@ function MyPage() {
           {isSharingClicked && data.simpleSharingList && data.simpleSharingList.map((item, index) => (
             <div key={index}>
               <div className="my_writing">
-                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '70%', fontSize: '11px'}}>
+                  <Link to={`/sharing/${item.id}`}>{item.title}</Link>
+                </div>
                 <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
               </div>
             </div>
@@ -185,7 +194,9 @@ function MyPage() {
           {isGroupbuyingClicked && data.simpleGroupBuyingList && data.simpleGroupBuyingList.map((item, index) => (
             <div key={index}>
               <div className="my_writing">
-                <div style={{width: '70%', fontSize: '11px'}}>{item.title}</div>
+                <div style={{width: '70%', fontSize: '11px'}}>
+                  <Link to={`/groupBuying/${item.id}`}>{item.title}</Link>
+                </div>
                 <div style={{width: '30%', display: 'flex', justifyContent: 'flex-end', color: '#737373', fontSize: '11px'}}>{item.postTime}</div>
               </div>
             </div>
